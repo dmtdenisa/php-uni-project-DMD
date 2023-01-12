efc78e1da1f97b52b9a3d065ca90be613a85b6e5
Denisa <dumitrumaria10@gmail.com>
2023-01-12 21:18:16 +0200

    completed dashboard and basic navigation

--- app/main-app/dashboard.php
+++ app/main-app/dashboard.php
@@ -15,49 +15,32 @@ session_start();
 <?php include_once("header.php") ?>
 
 <body>
-    <main class="h-screen">
-        <div class="cards-container flex flex-row flex-wrap gap-x-11 gap-y-16 pt-20 px-40">
-            <div class="card w-64 h-52 flex flex-col cursor-pointer hover:scale-105">
-                <div class="card-image h-2/3 bg-accent border rounded-t-2xl border-none">
-                    <img src="https://bloody-disgusting.com/wp-content/uploads/2017/09/salems-lot.jpg" class="border rounded-t-2xl border-none">
-                </div>
-                <div class="card-description h-1/3 bg-white border border-hidden rounded-b-2xl flex flex-col">
-                    <div class="card-title h-1/3 w-full border-hidden pt-2 px-4 text-xs">Salem's Lot</div>
-                    <div class="card-info h-2/3 border rounded-b-2xl border-hidden flex flex-row">
-                        <div class="card-rating flex flex-row w-2/3 items-center pl-4">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                        </div>
-                        <div class="card-watched w-1/3 flex flex-row items-center justify-end pr-4">
-                            <span class="border rounded-full w-5 h-5 border-none"><img src="../assets/hidden.png" class=" h-5 w-5"></span>
-                        </div>
-                    </div>
-                </div>
+    <main>
+        <div class="cards-container flex flex-column flex-wrap gap-y-4 pt-16 pb-0">
+            <div class="flex flex-row w-full justify-center space-x-4">
+                <a href="./movies.php" class="card w-80 h-64 flex flex-col justify-center items-center gap-y-5 cursor-pointer hover:scale-105 shadow-md bg-gray-50">
+                    <h1 class=" text-2xl font-extrabold font-sans">Movies</h1>
+                    <img class=" w-24" src="../assets/clapperboard.png">
+                </a>
+
+                <a href="./books.php" class="card w-80 h-64 flex flex-col justify-center items-center gap-y-5 cursor-pointer hover:scale-105 shadow-md bg-gray-50">
+                    <h1 class=" text-2xl font-extrabold font-sans">Books</h1>
+                    <img class=" w-24" src="../assets/books.png">
+                </a>
             </div>
 
-            <div class="card w-64 h-52 flex flex-col">
-                <div class="card-image h-2/3 bg-accent border rounded-t-2xl border-none">
-                    <img src="https://media.timeout.com/images/103625898/image.jpg" class="border rounded-t-2xl border-none">
-                </div>
-                <div class="card-description h-1/3 bg-white border border-hidden rounded-b-2xl flex flex-col">
-                    <div class="card-title h-1/3 w-full border-hidden pt-2 px-4 text-xs">The Wailing</div>
-                    <div class="card-info h-2/3 border rounded-b-2xl border-hidden flex flex-row">
-                        <div class="card-rating flex flex-row w-2/3 items-center pl-4">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                            <img class="w-5 h-5" src="../assets/star.png">
-                        </div>
-                        <div class="card-watched w-1/3 flex flex-row items-center justify-end pr-4">
-                            <span class="border rounded-full w-5 h-5 border-none"><img src="../assets/eye.png" class=" h-5 w-5"></span>
-                        </div>
-                    </div>
-                </div>
+            <div class="flex flex-row w-full justify-center space-x-4">
+                <a href="./articles.php" class="card w-80 h-64 flex flex-col justify-center items-center gap-y-5 cursor-pointer hover:scale-105 shadow-md bg-gray-50">
+                    <h1 class=" text-2xl font-extrabold font-sans">Articles</h1>
+                    <img class=" w-24" src="../assets/newspaper.png">
+                </a>
+
+                <a href="./albums.php" class="card w-80 h-64 flex flex-col justify-center items-center gap-y-5 cursor-pointer hover:scale-105 shadow-md bg-gray-50">
+                    <h1 class=" text-2xl font-extrabold font-sans">Albums</h1>
+                    <img class=" w-24" src="../assets/music-album.png">
+                </a>
             </div>
+
         </div>
     </main>
 </body>
--- app/main-app/header.php
+++ app/main-app/header.php
@@ -13,19 +13,19 @@
 <body class="neutral-content h-screen drop-shadow">
     <div class="navbar bg-base-100">
         <div class="flex-1">
-            <div class="btn btn-ghost normal-case text-xl">
+            <a class="btn btn-ghost normal-case text-xl" href="./dashboard.php">
                 <div class="flex flex-row gap-3">
-                <img src="../assets/treasure.png" class="w-6">
-                <span class="font-sans font-bold">myLittleArchive</span>
+                    <img src="../assets/treasure.png" class="w-6">
+                    <span class="font-sans font-bold">myLittleArchive</span>
                 </div>
-            </div>
+            </a>
         </div>
         <div class="flex-none">
             <ul class="menu menu-horizontal px-1">
                 <li tabindex="0">
-                <?php
-                if (isset($_SESSION['user'])) {
-                    echo "
+                    <?php
+                    if (isset($_SESSION['user'])) {
+                        echo "
                     <a>
                     Archives
                     <svg class=\"fill-current\" xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 24 24\">
@@ -33,16 +33,18 @@
                     </svg>
                 </a>
                 <ul class=\"p-2 bg-base-100 w-32\">
-                    <li><a>Movies</a></li>
-                    <li><a>Articles</a></li>
+                    <li><a href='./movies.php'>Movies</a></li>
+                    <li><a href='./books.php'>Books</a></li>
+                    <li><a href='./articles.php'>Articles</a></li>
+                    <li><a href='./albums.php'>Albums</a></li>
                 </ul>
                     ";
-                    echo "<li><a href='../includes/logout.inc.php' class=\"hover:bg-secondary hover:text-slate-50\">LOGOUT</a></li>";
-                }
-                 ?>
-                    
+                        echo "<li><a href='../includes/logout.inc.php' class=\"hover:bg-secondary hover:text-slate-50\">LOGOUT</a></li>";
+                    }
+                    ?>
+
                 </li>
-                
+
             </ul>
         </div>
     </div>
